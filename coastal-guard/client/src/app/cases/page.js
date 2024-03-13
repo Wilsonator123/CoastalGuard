d8e769180228b4064a9cae1116442f61190f6337
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-03-13 19:35:19 +0000

    Rebase with main

---
+++ coastal-guard/client/src/app/cases/page.js
@@ -0,0 +1,19 @@
+import CasesTable from './CasesTable';
+
+const fetchData = async () => {
+    const res = await fetch(process.env.API_URL + '/get-cases');
+    const data = await res.json();
+    return data;
+}
+export default async function Page() {
+    const data = await fetchData();
+
+    return (
+        <CasesTable data={data}/>
+    );
+
+}
+
+
+
+
